8e1011584cc50438dc21bce8127ca0b571b21d19
Kirill <maenkov.k@tk23.ru>
2023-04-18 13:53:49 +0400

    refactored

--- app/src/main/java/com/example/mvi/utils/Mapper.kt
+++ app/src/main/java/com/example/mvi/utils/Mapper.kt
@@ -3,26 +3,28 @@ package com.example.mvi.utils
 import com.example.mvi.R
 import com.example.mvi.screens.list.contract.ListContract
 import com.example.mvi.screens.user.contract.UserContract
+import com.example.mvi.screens.user.models.User
 
 object Mapper {
 
     object ResponseToState {
 
-        fun Result<List<String>?>.toListState(): ListContract.State = fold(
-            onSuccess = { data ->
-                if (data.isNullOrEmpty()) ListContract.State.Empty
-                else ListContract.State.Loaded(data = data)
-            },
-            onFailure = { throwable ->
-                ListContract.State.Failure(
-                    message = throwable.localizedMessage?.let { message ->
-                        UiText.DynamicString(value = message)
-                    } ?: UiText.StringResource(resId = R.string.unknown_exception)
-                )
-            }
-        )
+        fun Result<List<String>?>.toListState(): ListContract.State =
+            fold(
+                onSuccess = { data ->
+                    if (data.isNullOrEmpty()) ListContract.State.Empty
+                    else ListContract.State.Loaded(data = data)
+                },
+                onFailure = { throwable ->
+                    ListContract.State.Failure(
+                        message = throwable.localizedMessage?.let { message ->
+                            UiText.DynamicString(value = message)
+                        } ?: UiText.StringResource(resId = R.string.unknown_exception)
+                    )
+                }
+            )
 
-        fun Result<String>.toUserState(): UserContract.State = fold(
+        fun Result<User?>.toUserState(): UserContract.State = fold(
             onSuccess = { data ->
                 UserContract.State.Loaded(data = data)
             },
